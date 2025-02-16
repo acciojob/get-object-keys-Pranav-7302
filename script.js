ee7608d0ee63946eca35a3adfa53f32fda42b572
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-02-16 09:59:47 +0530

    Adder geyKeys function & updated cypress tests:

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 const student ={
-    name: "Johny",
-    age: "12",
-    city: "varanasi"
+    name: "John",
 }
 
 function getKeys(obj){
 return Object.keys(obj)
 }
 console.log(getKeys(student))
+
+export { student, getKeys };
