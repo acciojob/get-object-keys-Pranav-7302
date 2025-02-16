1a50ba08717aa6598a963472139408139d6abfa4
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-02-16 09:31:48 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student ={
+    name: "Johny",
+    age: "12",
+    city: "varanasi"
+}
+
+function getKeys(obj){
+return Object.keys(obj)
+}
+console.log(getKeys(student))
