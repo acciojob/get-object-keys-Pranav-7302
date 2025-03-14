f30c130e47a942161273bd90ee943c8313a6896b
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-03-14 15:29:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Define the student object globally
-window.student = {
-  name: "John"
+// Create the student object
+const student = {
+  name: "John",
+  getKeys: function () {
+    return Object.keys(this);  // "this" refers to student object
+  }
 };
 
-// Function to get all keys from the object
+// Function to get all keys from any object
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
 // Do not change the code below
+window.student = student;
 window.getKeys = getKeys;
