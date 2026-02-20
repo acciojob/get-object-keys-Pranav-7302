1b7a0d535415224d0d5ad1e211add1a9a79ef401
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2026-02-20 17:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // Create the student object
 const student = {
   name: "John",
-  getKeys: function () {
-    return Object.keys(this);  // "this" refers to student object
-  }
+  
 };
 
 // Function to get all keys from any object
 function getKeys(obj) {
-  return Object.keys(obj);
+	let arr = Object.keys(obj);
+  return arr ;
 }
 
-// Do not change the code below
-window.student = student;
-window.getKeys = getKeys;
+
